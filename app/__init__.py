9546308f0e36aa74438724644352a228351a1316
FlozyTess <cecilrency23@gmail.com>
2025-04-06 05:40:11 +0300

    updated

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes import api
+    app.register_blueprint(api)
+
+    return app
+
+app = create_app()
+
